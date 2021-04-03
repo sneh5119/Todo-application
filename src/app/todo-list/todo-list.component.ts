106da12d933fec1ef3bfb36a917b7dc45a81a6ce
Snehjeet kaur <sneh5119@gmail.com>
2021-04-03 17:35:29 +0530

    Pushed tasks

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Todo } from '../Todo';
 import { TodoService } from '../todo.service';
 
@@ -11,6 +11,7 @@ export class TodoListComponent implements OnInit {
 
   todos:Todo[]=[];
   constructor(private todoService : TodoService) { }
+  @ViewChild('box') inputName;
 
   ngOnInit(): void {
      this.getTodosData();
@@ -20,6 +21,24 @@ export class TodoListComponent implements OnInit {
   getTodosData(){    
     this.todos=this.todoService.getTodos();
     console.log("todos array =>> ",this.todos)
+  }
+
+  addTask(val){
+    let count=this.todos.length+1;
+    let task={
+       id:count++,
+       description:val
+     }
+     this.todos.push(task);
+     this.inputName.nativeElement.value='';
+  }
+
+  delete(){
+    console.log("deleted");
+  }
+
+  edit(){
+    console.log("edited");
   }
 
 }
